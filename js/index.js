8c4b7c9abfe76d87360ea71279b47973583819e0
Jelle Wallenburg <jellewallenburg@gmail.com>
2023-05-02 18:27:39 +0200

    iterations 1-2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
-  };
 
-  function startGame() {}
+  function startGame() {
+    const ctx = document.getElementById('canvas').getContext('2d');
+    const roadImg = new Image();
+    roadImg.src = './images/road.png';
+    roadImg.onload = function () {ctx.drawImage(roadImg, 0, 0, 500, 700)};
+
+    const carImg = new Image();
+    carImg.src = './images/car.png'
+    carImg.onload = function () {ctx.drawImage(carImg, 200, 500, 100, 200)}
+      };
+
+
+  }
 };
+
+
+
+
+
+
+
